9e8e6d180fafd97e9777c0f46567b02666d6ad87
Treinamento <Treinamento@RD723923.raiadrogasil.com>
2020-02-14 11:46:02 -0300

    Parte F feita!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Date data = new Date();
+
+
+        System.out.println(data);
+    }
+}
---
+++ src/Matricula.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Matricula {
+
+    private Aluno aluno;
+    private Curso curso;
+    private Date dataMatricula;
+
+
+    Matricula(){
+    }
+
+
+    public Matricula(Aluno aluno, Curso curso) {
+        this.aluno = aluno;
+        this.curso = curso;
+        dataMatricula = new Date();
+    }
+
+
+}
