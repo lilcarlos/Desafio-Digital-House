01117d641579ac9d1a1ceee293107348de387ef5
Treinamento <Treinamento@RD723923.raiadrogasil.com>
2020-02-14 11:20:58 -0300

    Parte D feita!

---
+++ src/ProfessorAdjunto.java
@@ -0,0 +1,23 @@
+public class ProfessorAdjunto extends Professor {
+
+    private int horasDeMonitoria;
+
+
+    public ProfessorAdjunto () {
+
+    }
+
+    public ProfessorAdjunto(String nome, String sobrenome, int tempoDeCasa, int codigoProfessor, int horasDeMonitoria) {
+        super(nome, sobrenome, tempoDeCasa, codigoProfessor);
+        this.horasDeMonitoria = horasDeMonitoria;
+    }
+
+
+    public int getHorasDeMonitoria() {
+        return horasDeMonitoria;
+    }
+
+    public void setHorasDeMonitoria(int horasDeMonitoria) {
+        this.horasDeMonitoria = horasDeMonitoria;
+    }
+}
